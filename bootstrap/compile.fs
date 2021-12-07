d9b287c125a2f39a7add4ae6efa1eeab4caeea7b
Koichi Nakamura <koichi@idein.jp>
2021-12-07 20:01:06 +0900

    Add bootstrap/lexer.fs

--- bootstrap/compile.fs
+++ bootstrap/compile.fs
@@ -1,1 +1,9 @@
-." I'll write a compiler here!" cr
+\ planckvm -
+\ Copyright (C) 2021 nineties
+
+\ This is an implementation of PlanckVM compiler
+\ in PlanckForth.
+\ It compiles PlanckIR programs to object files
+\ for the vm.
+
+include lexer.fs
---
+++ bootstrap/lexer.fs
@@ -0,0 +1,7 @@
+\ planckvm -
+\ Copyright (C) 2021 nineties
+
+\ Lexer of PlanckIR tokens.
+\ See spec/syntax.rst.
+
+." I'm a lexer" cr
