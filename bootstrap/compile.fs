6fab52ae74e81390f431f843cc7d2f00744aa7b5
Koichi Nakamura <koichi@idein.jp>
2021-12-20 07:14:35 +0900

    writing codegen

--- bootstrap/compile.fs
+++ bootstrap/compile.fs
@@ -11,6 +11,15 @@ include parser.fs
 struct
 end-struct compiler%
 
+0
+    enum SecIdT
+drop
+
+struct
+    int% field section>type
+    int% field section>bytes
+end-struct section%
+
 private{
 
 \ Construct Control-Flow Graph from an array of basic blocks
@@ -34,19 +43,29 @@ private{
     endcase
 ;
 
-: compile-program ( program .. )
-    make-compiler swap
-    program>defs @ dup array-size 0 ?do
-        i over array@ 2 pick compile-definition
-    loop
+: compile-program ( program -- obj )
+    \ make-compiler swap
+    \ program>defs @ dup array-size 0 ?do
+    \     i over array@ 2 pick compile-definition
+    \ loop
+    drop 0
+; export
+
+: codegen ( obj file -- )
+    s" PLNK" 4 2 pick write-file throw
+    4 sp@ 4 3 pick write-file throw
     2drop
 ; export
 
 }private
+
+s" test.pk" W/O open-file throw constant testfile
 
 s" function main(): i32 {
 block:
     return
 }
 "
-make-string parse compile-program
+make-string parse compile-program testfile codegen
+
+testfile flush-file throw
